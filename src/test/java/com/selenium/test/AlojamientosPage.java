4932245ee22931672230db03ff0cf1cc5a4b1b10
AndresBargas <candresbargas@gmail.com>
2022-09-28 13:23:08 -0300

    entregas restantes

--- src/test/java/com/selenium/test/AlojamientosPage.java
+++ src/test/java/com/selenium/test/AlojamientosPage.java
@@ -1,42 +1,175 @@
 package com.selenium.test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class AlojamientosPage extends Test_Despegar {
-	public WebDriver driver;
-	WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(1));
+import com.selenium.driver.DriverFactory;
+
+public class AlojamientosPage{
+	WebDriver driver=null;
+	WebDriverWait wait;
 	
-	By cerrar=By.xpath("/html/body/nav/div[4]/div[1]/i");
-	By aceptar = By.xpath("//*[@id=\"lgpd-banner\"]/div/a[2]/em");
-	By alojamientos = By.xpath("//body/nav[1]/div[2]/div[1]/div[3]/ul[1]/li[1]/a[1]/div[1]/i[1]");
+	@FindBy(xpath="/html/body/nav/div[4]/div[1]/i") WebElement cerrar;
+	@FindBy(xpath="//*[@id=\"lgpd-banner\"]/div/a[2]/em") WebElement aceptar;
+	@FindBy(xpath="//body/nav[1]/div[2]/div[1]/div[3]/ul[1]/li[1]/a[1]/div[1]/i[1]") WebElement alojamientos;
+	@FindBy(xpath="//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div/div/div[3]/div[1]/div/div/div/input") WebElement destino;
+	@FindBy(xpath="//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div/div/div[3]/div[2]/div/div[1]/div/div/div/div/input") WebElement searchEntrada;
+	@FindBy(xpath="//*[@id=\"component-modals\"]/div[4]/div[1]/div[2]/div[1]/div[3]/div[29]/div") WebElement fechaEntrada;
+	@FindBy(xpath="//*[@id=\"component-modals\"]/div[4]/div[1]/div[2]/div[1]/div[3]/div[30]/div") WebElement fechaSalida;
+	@FindBy(xpath="//*[@id=\"component-modals\"]/div[4]/div[2]/div[1]/button/em") WebElement aceptarFecha;
+	@FindBy(xpath="//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div/div/div[3]/div[3]/div/div/div[1]/input") WebElement boxHabitaciones;
+	@FindBy(xpath="//*[@id=\"component-modals\"]/div[5]/div/div/div[1]/div[2]/div[1]/div[2]/div/button[2]") WebElement selectAdultos;
+    @FindBy(xpath="//*[@id=\"component-modals\"]/div[5]/div/div/div[1]/div[2]/div[2]/div[2]/div/button[2]") WebElement selectMenores;
+	@FindBy(xpath="//*[@id=\"component-modals\"]/div[5]/div/div/div[2]/a[1]/em") WebElement aplicar;
+	@FindBy(xpath="//*[@id=\"searchbox-sbox-box-hotels\"]/div/div/div/div/div[3]/button") WebElement buscar;
+	@FindBy(xpath="/html/body/aloha-app-root/aloha-results/div/div/div/div[2]/div[2]/aloha-list-view-container/div[3]/div[1]/aloha-cluster-container/div/div") WebElement primerOpcion;
+	@FindBy(xpath="/html/body/nav/div[2]/div/div[3]/ul/li[6]/a/div/i") static WebElement vActividades;
+	@FindBy(css="ul.header-list-products>li>a") List<WebElement> list ;
+	
 
 	public AlojamientosPage(WebDriver driver) {
-		
-		
-		
-		super(driver);
-		// TODO Auto-generated constructor stub
+		this.driver=driver;
+		PageFactory.initElements(this.driver, this);
 	}
 	
-	public void ingresarAlojamientos() throws InterruptedException {
-		
+	public void cerrarSup(){
+		wait=new WebDriverWait(driver,Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.visibilityOf(cerrar));
+		cerrar.click();
+	}
 	
-		click(cerrar); wait.until(ExpectedConditions.visibilityOfElementLocated(cerrar));
+	public void cerrarInf(){
 		
-		click(aceptar); wait.until(ExpectedConditions.visibilityOfElementLocated(aceptar));
+		wait.until(ExpectedConditions.visibilityOf(aceptar));
+		aceptar.click();
+	}
+	
+	public void clickAlojamientos() {
+
+	wait.until(ExpectedConditions.visibilityOf(alojamientos));
+	try {
 		Thread.sleep(1000);
-		if(isDisplayed(alojamientos)) {
-			click(alojamientos);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	alojamientos.click();
+	
+	}
+	
+	public void ingresoDestino(String prov)  {
+		
+			destino.click(); try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			destino.sendKeys(prov); try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-		}else {
-			System.out.println("Page no encontrado");
+			destino.sendKeys(Keys.ENTER); try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	
+	public void seleccionarFechas() {
+
+		
+		searchEntrada.click(); 
+		
+		wait.until(ExpectedConditions.visibilityOf(fechaEntrada));
+		fechaEntrada.click(); try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		wait.until(ExpectedConditions.visibilityOf(fechaSalida));
+		fechaSalida.click(); try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		wait.until(ExpectedConditions.visibilityOf(aceptarFecha));
+		aceptarFecha.click();
+		
 	}
-
+	
+	public void seleccionarHabitaciones() {
+		wait.until(ExpectedConditions.visibilityOf(boxHabitaciones));
+		boxHabitaciones.click(); try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		wait.until(ExpectedConditions.visibilityOf(selectAdultos));
+		selectAdultos.click();
+		
+		wait.until(ExpectedConditions.visibilityOf(selectMenores));
+		selectMenores.click();
+		
+		Select select = new Select (driver.findElement(By.xpath("//*[@id=\"component-modals\"]/div[5]/div/div/div[1]/div[2]/div[3]/div[2]/div/div/select")));
+		select.selectByIndex(2);
+		
+		wait.until(ExpectedConditions.visibilityOf(aplicar));
+		aplicar.click();
+	}
+	public void buscarDestino() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		wait.until(ExpectedConditions.visibilityOf(buscar));
+		buscar.click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void visualizarDestino() {
+		wait.until(ExpectedConditions.visibilityOf(primerOpcion));
+		primerOpcion.click();
+	}
+	
+	public static void handlesFocus(WebDriver driver, int index) {
+		ArrayList<String> listHandles = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(listHandles.get(index));
+		vActividades.click();
+	}
+	public void recLista() {
+		for(WebElement i:list) {
+			System.out.println(i.getAttribute("href"));
+		}
+	}
+	
 }
--- src/test/java/com/selenium/test/Alojamientos_Test.java
+++ src/test/java/com/selenium/test/Alojamientos_Test.java
@@ -3,28 +3,54 @@ package com.selenium.test;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 
 public class Alojamientos_Test {
 	
-  private WebDriver driver;
-  AlojamientosPage alojamientosPage;
+  WebDriver driver=null;
+  AlojamientosPage alojamientosPage=null;
  
-  @BeforeMethod
+  @BeforeMethod(alwaysRun=true)
   public void setUp() throws Exception {
-	  alojamientosPage = new AlojamientosPage(driver);
-	  driver = alojamientosPage.chromeDriverConnection();
-	  alojamientosPage.visit("https://www.despegar.com.ar/");
+	  
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\Usuario\\drivers\\chromedriver.exe");
+	  this.driver=new ChromeDriver();
+	  this.driver.get("https://www.despegar.com.ar/");
   }
 
-  @AfterMethod
+  @AfterMethod(alwaysRun=true)
   public void afterMethod() {
+	 driver.quit();
+  }
+  
+  @DataProvider
+  public Object[][] data(){
+	return new Object[][] {
+		{"Salta"},
+		{"Mendoza"}
+	};
+	  
   }
 
+  @Test(groups="grupo_1", dataProvider="data")
+  public void alojamientos_test(String provincia) throws InterruptedException{
 
-  public void test() throws InterruptedException{
-	  alojamientosPage.ingresarAlojamientos();
+	  alojamientosPage = new AlojamientosPage(this.driver);
+	  alojamientosPage.cerrarSup();
+	  alojamientosPage.cerrarInf();
+	  alojamientosPage.clickAlojamientos();
+	  alojamientosPage.ingresoDestino(provincia);
+	  alojamientosPage.seleccionarFechas();
+	  alojamientosPage.seleccionarHabitaciones();
+	  alojamientosPage.buscarDestino();
+	  alojamientosPage.visualizarDestino();
+	  AlojamientosPage.handlesFocus(this.driver, 1);
+	  alojamientosPage.recLista();
   }
  
+  
+  
+  
 }
